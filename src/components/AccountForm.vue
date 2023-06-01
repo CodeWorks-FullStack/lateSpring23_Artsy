0446ca4d790462d8c6444aac42fd469bec6b7e3c
Jake Overall <joverall22@gmail.com>
2023-06-01 10:36:28 -0600

    adds default form component

---
+++ src/components/AccountForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+
+
+    <!-- <input type="text" required v-model="editable.name">
+    <input type="text" required v-model="editable.picture">
+    <input type="text" required v-model="editable.coverImg"> -->
+
+
+
+  </form>
+</template>
+
+
+<script>
+import { ref, watchEffect } from 'vue';
+import { AppState } from '../AppState.js';
+import Pop from '../utils/Pop.js';
+
+export default {
+  setup() {
+    const editable = ref({})
+
+    watchEffect(() => {
+      editable.value = { ...AppState.account }
+    })
+
+
+    return {
+      editable,
+      async handleSubmit() {
+        try {
+
+        } catch (error) {
+          Pop.error(error, '[Editing Account]')
+        }
+      }
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
